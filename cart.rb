f31b510fb2f00ba3dca71ff1d5ddd06b558a337f
Jordão Frazão <jordaofrazao@gmail.com>
2018-03-16 19:45:52 -0300

    Fixing errors on remove methods.

--- cart.rb
+++ cart.rb
@@ -2,37 +2,43 @@ class Cart
   require_relative "product.rb"
   def initialize
     @product_list = []
-    order = []
+    @order = []
   end
 
   def add_product_to_the_cart(name, price)
     product = Product.new(name, price)
-    @product_list << product_list
+    @product_list << product
     puts "Product added to the cart!"
   end
 
   def remove_product_from_the_cart(product_name)
-    @product_list.include? @product.product_name
-    @product_list.delete_if {|product| product.name = product_name}
-    puts "Product removed from the cart."
+    @product_list.map do |product|
+      if product.name == product_name
+        @product_list.delete(product)
+        puts "Product removed from the cart."
+      end
+    end
   end
 
   def show_product_list
     @product_list.each do |product|
-      puts product_list
+      puts product
     end
   end
 
   def new_order(product_name)
-    @product_list.include? @product.product_name
-    @order << @product
-    puts "Product added to the order!"
+    @product_list.map do |product|
+      if product.name == product_name
+        @order << product
+        puts "Product added to the order!"
+      end
+    end
   end
 
   def add_product_to_the_order(product_name)
     @product_list.map do |product|
       if product.name == product_name
-        @order << @product
+        @order << product
         puts "Product added to the order!"
       elsif puts "Product is not on the product list!"
       end
@@ -51,7 +57,7 @@ class Cart
 
   def show_products_on_the_order
     @order.each do |product|
-      puts product_list
+      puts product
     end
   end
 
